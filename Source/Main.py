27bbcbe05f4c7ffa069b4e4b88a399b5191a962d
Angel Motta <angelmotta@gmail.com>
2020-06-27 16:37:13 -0500

    images test face

--- Source/Main.py
+++ Source/Main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+import face_recognition
+
+q_imagen = face_recognition.load_image_file("fotos_test/vizcarraTest.jpg")
+
+q_imagen_encoding = face_recognition.face_encodings(q_imagen)[0]
+
+print(q_imagen_encoding)
