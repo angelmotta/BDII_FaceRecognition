e892b506fac0db774f53ba72c496a8b516d4ee2b
Angel Motta <angelmotta@gmail.com>
2020-06-28 17:54:18 -0500

    fix face_encoding and dataSet experiments

--- Source/Extraccion.py
+++ Source/Extraccion.py
@@ -2,20 +2,30 @@ import os
 import face_recognition
 
 
-def genCaracteristicas():
+def genCaracteristicas(dirPics):
+    print("--- Get Caracteristicas from Pics ---")
     dataDB = []
-    dirPics = 'fotos_bd'
     listPics = os.listdir(dirPics)
     for image in listPics:
         print(dirPics + "/" + image)
         q_imagen = face_recognition.load_image_file(dirPics+"/"+image)
-        q_imagen_encoding = face_recognition.face_encodings(q_imagen)[0]
-        print(q_imagen_encoding)
-        dataDB.append((image, q_imagen_encoding))
+        q_imagen_encoding = face_recognition.face_encodings(q_imagen)
+        if len(q_imagen_encoding) > 0:
+            q_imagen_encoding = q_imagen_encoding[0]
+            #print(q_imagen_encoding)
+            dataDB.append((image, q_imagen_encoding))
+        else:
+            print("No faces found in the image!")
     return dataDB
 
 
 def genCaracPic(filePic):
+    print("--- Get Caracteristicas from: ", filePic, "----")
     q_imagen = face_recognition.load_image_file(filePic)
-    q_imagen_encoding = face_recognition.face_encodings(q_imagen)[0]
-    return q_imagen_encoding
+    q_imagen_encoding = face_recognition.face_encodings(q_imagen)
+    if len(q_imagen_encoding) > 0:
+        q_imagen_encoding = q_imagen_encoding[0]
+        return q_imagen_encoding
+    else:
+        print("No faces found in the image!")
+        return []
